8eb8f02ceb9345d024f86f7fa08e48df6126d6b1
Jian-Long Huang <huang@jianlong.org>
2019-07-28 12:08:06 +0800

    First commit

---
+++ app.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         巴哈姆特-移除小屋背景圖
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  移除巴哈小屋背景圖
+// @author       jlhg
+// @match        https://home.gamer.com.tw/homeindex.php?*
+// @match        https://home.gamer.com.tw/bahawall.php?*
+// @match        https://home.gamer.com.tw/creation.php?*
+// @match        https://home.gamer.com.tw/bookmark.php?*
+// @match        https://home.gamer.com.tw/acgbox.php?*
+// @match        https://home.gamer.com.tw/joinGuild.php?*
+// @match        https://home.gamer.com.tw/creationDetail.php?*
+// @match        https://home.gamer.com.tw/creationCategory.php?*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    document.body.style.background = 'none';
+})();
